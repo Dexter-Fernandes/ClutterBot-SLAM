5659a626ab9bb4a808fad772f3e9349d3ad3b558
Dexter-Fernandes <dexterfernandes11@gmail.com>
2025-09-04 17:01:00 +0000

    Created my_world.sdf and added to launch file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -9,32 +9,47 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-    
-    package_name='my_bot'
-    
+
+    package_name = "my_bot"
+
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-        )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
-    
+
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-        )])
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("ros_gz_sim"),
+                    "launch",
+                    "gz_sim.launch.py",
+                )
+            ]),
+            launch_arguments={
+                "gz_args": os.path.join(
+                    get_package_share_directory(package_name), "worlds", "my_world.sdf"
+                )
+            }.items(),
     )
-    
-    spawn_entity = Node(package='ros_gz_sim', executable='create', arguments=[
-        "-name", "my_bot",
-        "-topic", "/robot_description",
-        "-z", "0.1"
-        ],
+
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-name", "my_bot", "-topic", "/robot_description", "-z", "0.1"],
     )
-    
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        TimerAction(period=2.0, actions=[spawn_entity]),
-    ])
+
+    return LaunchDescription(
+        [
+            rsp,
+            gazebo,
+            TimerAction(period=2.0, actions=[spawn_entity]),
+        ]
+    )
